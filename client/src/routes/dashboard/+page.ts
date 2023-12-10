7e64b9fe739ce2e90420c0354ebfd9d1fd561e89
Pataki Norbert <nonorbi2@gmail.com>
2023-12-10 22:38:11 +0200

    move url into .env

--- client/src/routes/dashboard/+page.ts
+++ client/src/routes/dashboard/+page.ts
@@ -2,15 +2,16 @@ import { redirect } from '@sveltejs/kit';
 import { getAccessToken } from '../../shared/helper';
 import type { PageLoad } from './$types';
 import { MAIN } from '../../shared/routes';
+import { PUBLIC_SERVER_BASE_URL } from '$env/static/public';
 
 export const load = (async ({ fetch }) => {
 	const accessToken = getAccessToken();
-
+	//console.log(SERVER_BASE_URL);
 	if (!accessToken) {
 		throw redirect(303, MAIN);
 	}
 
-	const response = await fetch('http://localhost:7777/employees/652d26915b600af263e46a65', {
+	const response = await fetch(`${PUBLIC_SERVER_BASE_URL}employees/652d26915b600af263e46a65`, {
 		headers: {
 			Authorization: `Bearer ${accessToken}`,
 			mode: 'cors'
