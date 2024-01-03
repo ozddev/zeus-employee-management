e0d711fcce58d3feaf7f741b2f9b45feff1c6ceb
Tamas99 <tamiballai9@gmail.com>
2024-01-03 16:50:15 +0200

    imported the JwtModule and the JwtStrategy provider in handler module

--- backend/gateway/src/handler/handler.module.ts
+++ backend/gateway/src/handler/handler.module.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common';
 import { HandlerService } from './handler.service';
 import { HandlerController } from './handler.controller';
 import { HttpModule } from '@nestjs/axios';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from '@auth/strategies/jwt.strategy';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: config.get<string | number>('JWT_EXPIRES'),
+          },
+        };
+      },
+    }),
+  ],
   controllers: [HandlerController],
-  providers: [HandlerService],
+  providers: [HandlerService, JwtStrategy],
 })
 export class HandlerModule {}
