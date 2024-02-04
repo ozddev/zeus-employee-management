1c647a95bc2091affca143018091b75200ca2ce9
Tamas99 <tamiballai9@gmail.com>
2024-02-04 23:43:00 +0200

    copied the gateway from the old backend

--- backend/apps/gateway/src/app/app.controller.ts
+++ backend/apps/gateway/src/app/app.controller.ts
@@ -1,8 +1,31 @@
-import { Controller, Get, Inject } from '@nestjs/common';
-
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
+import { Controller, Get, Inject, Post, Request, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
-@Controller()
+
+@Controller('auth')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('EMPLOYEES_SERVICE') private employeesClient: ClientProxy
+  ) {}
+
+  @Get('/')
+  hello() {
+    return 'Hello World!'
+  }
+
+  @Get('/hello')
+  helloworld() {
+    const pattern = { cmd: 'helloworld' };
+    const data = 'Alice';
+    return this.employeesClient.send(pattern, data);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.appService.login(req.user);
+  }
 }
--- backend/apps/gateway/src/app/app.module.ts
+++ backend/apps/gateway/src/app/app.module.ts
@@ -2,10 +2,37 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { LocalStrategy } from './strategies/local.strategy';
+import { HttpModule } from '@nestjs/axios';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      { name: 'EMPLOYEES_SERVICE', transport: Transport.TCP },
+    ]),
+    HttpModule,
+    PassportModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: config.get<string | number>('JWT_EXPIRES'),
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, LocalStrategy],
 })
 export class AppModule {}
